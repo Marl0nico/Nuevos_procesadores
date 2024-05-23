0307ecea660374c82b296c0c86b0bf7b713e79d2
Marlon Nicolalde <marlon.nicolalde@epn.edu.ec>
2024-05-23 10:35:25 -0500

    Clases padre e hijas listas, por completar instancias de figuras geométricas (5-12) en el main

--- Figuras Geometricas/src/Main.java
+++ Figuras Geometricas/src/Main.java
@@ -1,33 +1,17 @@
 public class Main {
     public static void main(String[] args) {
         Figuras_Geométricas figura1=new Figuras_Geométricas();
-        Círculos círculo1=new Círculos();
+        //objetos triángulos
         Triángulos triángulo1=new Triángulos();
-        Cuadriláteros cuadrilátero1=new Cuadriláteros();
-        Pentágono pentágono1=new Pentágono(5.45);
-        Pentágono pentágono2=new Pentágono();
-        Círculos círculo2= new Círculos(5.25);
         Triángulos triángulo2=new Triángulos(8.56, 12.67, 15.09);
+        //objetos cuadriláteros
+        Cuadriláteros cuadrilátero1=new Cuadriláteros();
         Cuadriláteros cuadrilátero2=new Cuadriláteros(3.56, 6.54);
-        Cuadriláteros cuadrilátero3=new Cuadriláteros(4.56, 2.45);
-        //círculo 1
-        System.out.println("Círculo 1");
-        círculo1.setRadio(4.65);
-        System.out.println(círculo1.getRadio());
-        círculo1.setNombre("Pileta CEC");
-        System.out.println(círculo1.getNombre());
-        círculo1.setRadio(8.45);
-        System.out.println("La nueva pileta del CEC tendrá un radio de:"+círculo1.getRadio());
-        System.out.println("Su área será:"+Double.parseDouble(String.format("%.2f", círculo1.calcularÁrea())));
-        System.out.println("Su perímetro es:"+Double.parseDouble(String.format("%.2f", círculo1.calcularPerímetro())));
-        //cuadrilátero 1
-        System.out.println("Cuadrilátero 1");
-        cuadrilátero1.setAncho(5.67);
-        cuadrilátero1.setLargo(8.09);
-        System.out.println("Largo:"+cuadrilátero1.getLargo()+"  Ancho:"+cuadrilátero1.getAncho());
-        System.out.println("Área:"+cuadrilátero1.calcularÁrea());
-        System.out.println("Perímetro:"+cuadrilátero1.calcularPerímetro());
-        //triángulo 1
+        //triángulos
+        triángulo1.setNombre("Figura geométrica: Triángulos");
+        System.out.println(triángulo1.getNombre());
+        triángulo1.setnLados(3);
+        System.out.println("Número de lados:"+triángulo1.getnLados());
         System.out.println("Triángulo 1");
         triángulo1.setBase(5.32);
         triángulo1.setAltura(7.39);
@@ -35,11 +19,27 @@ public class Main {
         System.out.println("Base:"+triángulo1.getBase()+"   Altura:"+triángulo1.getAltura());
         System.out.println("Área:"+triángulo1.calcularÁrea());
         System.out.println("Perímetro:"+triángulo1.calcularPerímetro());
-        //pentágono
-        System.out.println("Pentágono 1");
-        
-        //deber: leer Deitel sobre herencia y consultar y activar su cuenta estudiantil de github
-        //deber: terminar de hacer el ejercicio y probarlo en el main
-        //primer commit
+        System.out.println("Triángulo 2");
+        System.out.println("Base:"+triángulo2.getBase()+"   Altura:"+triángulo2.getAltura()+"   Hipotenusa:"+triángulo2.getHipotenusa());
+        System.out.println("Perímetro:"+triángulo2.calcularPerímetro());
+        System.out.println("Área:"+triángulo2.calcularÁrea());
+        System.out.println("-----------------------------------------");
+        //cuadriláteros
+        cuadrilátero1.setNombre("Figura geométrica: Cuadriláteros");
+        System.out.println(cuadrilátero1.getNombre());
+        cuadrilátero1.setnLados(4);
+        System.out.println("Número de lados:"+cuadrilátero1.getnLados());
+        System.out.println("Cuadrilátero 1");
+        cuadrilátero1.setAncho(5.67);
+        cuadrilátero1.setLargo(8.09);
+        System.out.println("Largo:"+cuadrilátero1.getLargo()+"  Ancho:"+cuadrilátero1.getAncho());
+        System.out.println("Área:"+cuadrilátero1.calcularÁrea());
+        System.out.println("Perímetro:"+cuadrilátero1.calcularPerímetro());
+        System.out.println("Cuadrilátero 2");
+        System.out.println("Largo:"+cuadrilátero2.getLargo()+"  Ancho:"+cuadrilátero2.getAncho());
+        System.out.println("Perímetro:"+cuadrilátero2.calcularPerímetro());
+        System.out.println("Área:"+cuadrilátero2.calcularÁrea());
+        System.out.println("-----------------------------------------");
+
     }
 }
